0c91dff179ae9a5248dfffb48e1462869505efc3
clab02 <belabrockmann@gmail.com>
2019-11-25 22:00:22 +0100

    Removed unnecessary console.log statement

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Task from './Task';
 export default function Tasks(props) {
-    console.log("Tasks")
     return (
         <div className="tasks">
             {props.tasks.map((item, index) => (<Task key={index} description={item.description} />))}
