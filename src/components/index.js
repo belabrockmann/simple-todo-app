defea686a0f2d1db0dc220c2260578f65a659e47
clab02 <belabrockmann@gmail.com>
2019-11-29 22:44:46 +0100

    Exported CreateTask component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Logo } from "./Logo";
 export { default as Tasks } from "./Tasks";
 export { default as EditTask } from "./EditTask";
+export { default as CreateTask } from "./CreateTask";
