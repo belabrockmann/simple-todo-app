82402e6feee0f3eaa730f9a6497147ddfdc0c1ad
clab02 <belabrockmann@gmail.com>
2019-11-29 22:48:37 +0100

    Added update mechanism

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Task from './Task';
 export default function Tasks(props) {
+    let tasks;
+    try {
+        tasks = localStorage.getItem("tasks");
+        if (tasks.indexOf(",") !== -1 || tasks.length) {
+            tasks = tasks.split(",");
+            console.log(tasks)
+        }
+        else {
+            tasks = [];
+            console.log("WHHHYY")
+        }
+    }
+    catch{
+        console.log("What")
+        tasks = [];
+        localStorage.setItem("tasks", tasks);
+    }
+    // eslint-disable-next-line
+    const [updater, setUpdater] = useState([]);
+    console.log(tasks)
     return (
         <div className="tasks">
             <h2 className="title">Your tasks</h2>
-            {props.tasks.map((item, index) => (<Task key={index} description={item.description} />))}
+            {tasks.length ? tasks.map((item, index) => (<Task key={index} description={item} deleteTask={props.deleteTask} setUpdater={setUpdater} />)) : <h3>You don't have any tasks</h3>}
         </div>
     );
 };
