1cc9442a1496a6ec10fee3bcf6ad050e81b7df40
clab02 <belabrockmann@gmail.com>
2019-11-30 22:38:41 +0100

    Added class toggle for disabled state

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 export default function Task(props) {
+    const [isTaskDisabled, setIsTaskDisabled] = useState(false);
     const deleter = () => {
         props.deleteTask(props.description);
         props.setUpdater([props.description, Math.random()]);
     }
+    const disableTask = e => {
+        setIsTaskDisabled(prevState => !prevState);
+    }
     return (
-        <div className="task">
+        <div className={isTaskDisabled ? "task task-disabled" : "task"}>
             <div className="checkbox-wrapper">
-                <input type="checkbox" className="checkbox" />
+                <input type="checkbox" className="checkbox" onClick={disableTask} />
             </div>
             <div className="description-wrapper"><p className="description">{props.description}</p></div>
             <Link className="task-button edit" to={`/edit/${props.description}`}></Link>
