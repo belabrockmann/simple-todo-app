aaa1c387c1c3158707ef9bf30a1b4b29d00381ff
clab02 <belabrockmann@gmail.com>
2019-11-30 22:40:08 +0100

    Added redirect after task creation

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
-
-export default function CreateTask(props) {
+import { withRouter } from 'react-router-dom';
+export default withRouter(function CreateTask(props) {
     const [newTask, setNewTask] = useState("");
+    const tasks = localStorage.getItem("tasks").split(",");
     const onClickHandler = e => {
-        if (newTask.length) {
+        e.preventDefault();
+        if (newTask.length && tasks.indexOf(newTask) === -1) {
             props.createNewTask(e, newTask);
             setNewTask("")
+            props.history.push("/")
         }
     };
     return (
@@ -13,9 +16,9 @@ export default function CreateTask(props) {
             <h2 className="title">Create new task</h2>
             <form className="task-description-form">
                 <label htmlFor="task-description-input" className="task-description-input-label">Task description</label>
-                <input type="text" className="task-description-input" id="task-description-input" onChange={e => { setNewTask(e.target.value) }} value={newTask} required={true} />
+                <input type="text" className="task-description-input" id="task-description-input" onChange={e => { setNewTask(e.target.value) }} value={newTask} required={true} autoFocus={true} />
                 <button className="btn btn-submit" onClick={onClickHandler}>Create task</button>
             </form>
         </div>
     )
-}
+})
