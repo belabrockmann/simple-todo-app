6df237b1874b68a39d0ccc9383f4413d252431ed
clab02 <belabrockmann@gmail.com>
2019-12-01 17:18:34 +0100

    Implemented taskstates

--- src/App.js
+++ src/App.js
@@ -4,29 +4,39 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 export default function App() {
-  let tasks;
+  let tasks, taskStates;
   try {
     tasks = localStorage.getItem("tasks");
+    taskStates = localStorage.getItem("taskStates");
     if (tasks.indexOf(",") !== -1) {
       tasks = tasks.split(",");
+      taskStates = taskStates.split(",");
     }
     else {
       tasks = [];
+      taskStates = [];
     }
   }
   catch{
     tasks = [];
+    taskStates = [];
     localStorage.setItem("tasks", tasks);
+    localStorage.setItem("taskStates", taskStates)
   }
   const createNewTask = (e, newTask, props) => {
     e.preventDefault();
     tasks.push(newTask);
+    taskStates.push(0);
     localStorage.setItem("tasks", tasks);
+    localStorage.setItem("taskStates", taskStates);
   }
 
   const deleteTask = (task) => {
-    tasks.splice(tasks.indexOf(task), 1);
+    const index = tasks.indexOf(task);
+    tasks.splice(index, 1);
+    taskStates.splice(index, 1);
     localStorage.setItem("tasks", tasks);
+    localStorage.setItem("taskStates", taskStates);
   }
   return (
     <>
