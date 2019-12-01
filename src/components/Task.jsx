bb2a62d5b417c334d883bce11e10a13de00bc750
clab02 <belabrockmann@gmail.com>
2019-12-01 17:17:55 +0100

    Added taskState handling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 export default function Task(props) {
-    const [isTaskDisabled, setIsTaskDisabled] = useState(false);
+    const tasks = localStorage.getItem("tasks").split(",");
+    const index = tasks.indexOf(props.description);
+    const taskStates = localStorage.getItem("taskStates").split(",");
+    const taskState = Number(taskStates[index]);
+    const [isTaskDisabled, setIsTaskDisabled] = useState(taskState);
     const deleter = () => {
         props.deleteTask(props.description);
         props.setUpdater([props.description, Math.random()]);
     }
     const disableTask = e => {
-        setIsTaskDisabled(prevState => !prevState);
+        taskStates[index] = (taskState + 1) % 2;
+        setIsTaskDisabled(prevState => (prevState + 1) % 2);
+        localStorage.setItem("taskStates", taskStates);
     }
     return (
         <div className={isTaskDisabled ? "task task-disabled" : "task"}>
             <div className="checkbox-wrapper">
-                <input type="checkbox" className="checkbox" onClick={disableTask} />
+                <input type="checkbox" className="checkbox" onChange={disableTask} checked={isTaskDisabled} />
             </div>
             <div className="description-wrapper"><p className="description">{props.description}</p></div>
             <Link className="task-button edit" to={`/edit/${props.description}`}></Link>
